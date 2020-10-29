edc39de48e245359caa37ea47f01d69fa09c45a8
danny <dannyzamora1996@gmail.com>
2020-10-29 13:41:22 -0700

    yolo

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: "uploads",
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
