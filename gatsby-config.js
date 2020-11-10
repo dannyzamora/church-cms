953dbd9be0ebdb523d87c87afd3022dd5df9f523
danny <dannyzamora1996@gmail.com>
2020-11-09 19:26:21 -0800

    fgsdfg

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,8 @@ module.exports = {
   /* Your site config here */
   plugins: [
     "gatsby-plugin-sass",
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
 
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
@@ -33,7 +32,15 @@ module.exports = {
           {
             resolve: `gatsby-remark-relative-images`,
             options: {
+              // [Optional] The root of "media_folder" in your config.yml
+              // Defaults to "static"
               staticFolderName: "static/img",
+              // [Optional] Include the following fields, use dot notation for nested fields
+              // All fields are included by default
+              include: ["featured"],
+              // [Optional] Exclude the following fields, use dot notation for nested fields
+              // No fields are excluded by default
+              exclude: ["featured.skip"],
             },
           },
 
