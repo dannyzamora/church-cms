f3ac316231c34c391ed38615cf99ed850dceb9eb
danny <dannyzamora1996@gmail.com>
2020-12-23 18:24:31 -0800

    changes

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -13,9 +13,8 @@ export const CalandarPageTemplate = ({}) => {
                 className="tile is-child  box"
                 src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=bzM5bDhlNW83MjUzMjRja2J0ajRqcnZqYWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23E67C73"
                 width="100%"
-                height="850"
                 frameBorder="0"
-                style={{ border: 0 }}
+                style={{ border: 0, height: "75vh" }}
                 allowFullScreen=""
                 aria-hidden="false"
                 tabIndex="0"
